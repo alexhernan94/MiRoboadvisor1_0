a244bafe34b2df0936cd646525bb514a3b1bae4a
Alex <alexhernan94@gmail.com>
2020-02-19 22:19:42 +0100

    Interfaz login 2

--- app/src/main/java/com/example/miroboadvisor1_0/MainActivity.java
+++ app/src/main/java/com/example/miroboadvisor1_0/MainActivity.java
@@ -3,12 +3,80 @@ package com.example.miroboadvisor1_0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.miroboadvisor1_0.model.EmailPasswordAuth;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText txtEmail;
+    private EditText txtPass;
+    private Button btnLogin;
+    private Button btnRegister;
+    private Button btnForgotPass;
+
+    private String email = "";
+    private String password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        txtEmail = (EditText) findViewById(R.id.editTextEmail);
+        txtPass = (EditText) findViewById(R.id.editTextPass);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+        btnForgotPass = (Button) findViewById(R.id.btn_newPass);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Both fields must be completed", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length()>=6) {
+                    Toast.makeText(MainActivity.this, "Password less than 6 characteres", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    mAuth.signInWithEmailAndPassword(email, password);
+                }
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = txtEmail.getText().toString();
+                password = txtPass.getText().toString();
+
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Both fields must be completed", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length()>=6) {
+                        Toast.makeText(MainActivity.this, "Password less than 6 characteres", Toast.LENGTH_SHORT).show();
+                    }
+                else{
+                    mAuth.createUserWithEmailAndPassword(email, password);
+                }
+            }
+        });
+
+        btnForgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.sendPasswordResetEmail(email);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/miroboadvisor1_0/model/EmailPasswordAuth.java
@@ -0,0 +1,50 @@
+package com.example.miroboadvisor1_0.model;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class EmailPasswordAuth {
+
+    private EditText txtEmail;
+    private EditText txtPass;
+    private Button btnLogin;
+    private Button btnRegister;
+    private Button btnForgotPass;
+
+    private String email = "";
+    private String password = "";
+
+    private static final String TAG = "EmailPassword";
+
+    private FirebaseAuth mAuth;
+
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "createUserWithEmail:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+
+            // ...
+        }
+    });
+
+}
